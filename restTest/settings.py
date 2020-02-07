a54d3ab31c988be34005f6791771837557695961
vladsakun <vlad120403@gmail.com>
2020-02-07 09:30:45 +0200

    requirements

--- restTest/settings.py
+++ restTest/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'rq95($0$hj()s_@9@52le5r@22p_b!4oeq6*2#534+)2&)xr0k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.169", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.1.169", "127.0.0.1", 'vlad12.pythonanywhere.com']
 
 # Application definition
 
