a344313500b9d4017b15a00f8a5192c6abaf923e
vladsakun <vlad120403@gmail.com>
2020-02-18 13:16:58 +0200

    Quiz front nad back

--- main/static/js/tabs_switcher.js
+++ main/static/js/tabs_switcher.js
@@ -32,18 +32,26 @@ $(window).ready(function () {
 
     Quiz.amount = $('.d-word').length;
 
+    var termElements = document.getElementsByClassName("term");
+
+    for (var i = 0; i < termElements.length; i++) {
+
+        Quiz.answers[termElements[i].textContent] = "empty";
+
+    }
+
     $(".answer").on("click", function () {
         $(this).parent().find('.answer').removeClass('active');
         $(this).addClass('active');
 
         var key = $(this).parent().parent().attr('data-id');
 
-        var term = $(this).parent().find('.term').text();
+        var term = $(this).parent().parent().find('.term').text();
 
         Quiz.answers[term] = $(this).text();
 
-        console.log($(this).text());
     });
+
     $(".finish").on("click", function () {
         $.ajax("/check/answers/", {
             method: 'POST',
@@ -53,7 +61,16 @@ $(window).ready(function () {
             }
         })
             .done((res) => {
-                console.log(res);
+                var mark = res.data.mark;
+                var questionDiv = $('#quiz-questions--div');
+                var questionDivHeight = questionDiv.height();
+                questionDiv.height(questionDivHeight);
+                $("#question-info--div").fadeOut("slow", function () {
+                    questionDiv.html('<div id="result--div">' + mark +
+                        '/' + Quiz.amount + ' </div>');
+
+                });
             });
     })
+
 });
--- main/views.py
+++ main/views.py
@@ -37,26 +37,45 @@ def get_dictation(request, code):
 def dictation(request, code):
     try:
         dictation = Dictation.objects.get(code=code)
-        dictation_words = json.loads(dictation.words)
+
+        all_dictation_words = json.loads(dictation.words)
+        other_words = all_dictation_words.copy()
         marked_words = json.loads(dictation.marked_words)
 
+        amount_of_questions = dictation.amount_of_words_for_dictation
+        amount_of_other_words = amount_of_questions - len(marked_words)
+
         for marked_word in marked_words:
-            dictation_words.append(marked_word)
+            all_dictation_words.append(marked_word)
 
         dictation_words_for_template = []
-        dictation_words_len = len(dictation_words) - 1
 
-        for i in range(0, dictation_words_len):
-            dictation_words_copy = dictation_words.copy()
-            three_random_values = get_random_dictation_words(dictation_words_copy, i)
-            answers = [dictation_words[i]["translation"],
+        for n in range(0, len(marked_words)):
+            three_random_values = get_random_dictation_words(all_dictation_words.copy(), marked_words[n])
+            answers = [marked_words[n]["translation"],
                        three_random_values[0],
                        three_random_values[1],
                        three_random_values[2]]
+
             random.shuffle(answers)
+
             dictation_words_for_template.append(
-                {"term": dictation_words[i]["term"],
-                 "translation": dictation_words[i]["translation"],
+                {"term": marked_words[n]["term"],
+                 "mixed": answers})
+
+        random.shuffle(other_words)
+
+        for i in range(0, amount_of_other_words):
+            three_random_values = get_random_dictation_words(all_dictation_words.copy(), other_words[i])
+            answers = [other_words[i]["translation"],
+                       three_random_values[0],
+                       three_random_values[1],
+                       three_random_values[2]]
+
+            random.shuffle(answers)
+
+            dictation_words_for_template.append(
+                {"term": other_words[i]["term"],
                  "mixed": answers})
 
         random.shuffle(dictation_words_for_template)
@@ -68,43 +87,68 @@ def dictation(request, code):
 
 
 def check_answers(request):
-    # answers = json.loads(request.data)
+    mark = 0
+
     dictation = Dictation.objects.get(id=request.POST.get("dictation_id"))
-    answers = request.POST.get('answers')
+
+    answers = json.loads(request.POST.get('answers'))
     dictation_words = json.loads(dictation.words)
     marked_words = json.loads(dictation.marked_words)
 
     for marked_word in marked_words:
         dictation_words.append(marked_word)
 
-    # for answer in answers:
-    #     answers[]
-    return JsonResponse(request)
+    prepared_dictation_words = {}
+
+    for dictation_word in dictation_words:
+        prepared_dictation_words[dictation_word["term"]] = dictation_word["translation"]
+
+    sorted_words_keys = sorted(prepared_dictation_words.keys(), key=lambda x: x.lower())
+    sorted_answers_keys = sorted(answers.keys(), key=lambda x: x.lower())
+
+    sorted_answers = {}
+    sorted_words = {}
+
+    for key in sorted_words_keys:
+        sorted_words.update({key: prepared_dictation_words[key]})
+
+    for key in sorted_answers_keys:
+        sorted_answers.update({key: answers[key]})
+
+    for answer_key in sorted_answers.keys():
+        if answers[answer_key] == sorted_words[answer_key]:
+            mark = mark + 1
+        else:
+            continue
+
+    user = User.objects.get(email=request.user.email)
+    try:
+
+        dictation_mark = DictationMark.objects.get(user=user, dictation=dictation)
+        dictation_mark.mark = mark
+
+    except DictationMark.DoesNotExist:
+
+        dictation_mark = DictationMark(user=user, dictation=dictation, mark=mark)
+
+    dictation_mark.save()
+    response_data = {
+        "data": {
+            "mark": mark
+        }
+    }
+    return JsonResponse(response_data, safe=False)
 
 
-def get_random_dictation_words(all_words, forloop_counter):
-    del all_words[forloop_counter]
+def get_random_dictation_words(all_words, forloop_object):
+    all_words.pop(all_words.index(forloop_object))
+    for word in all_words:
+        print(word["term"])
+    print("-" * 20)
     three_incorrect_values = []
     for i in range(0, 3):
-        random_index = random.randrange(0, len(all_words))
+        random_index = random.randrange(len(all_words))
 
         three_incorrect_values.append(all_words[random_index]["translation"])
         del all_words[random_index]
     return three_incorrect_values
-
-
-class DictationWord(object):
-
-    def __init__(self, term, translation, first_incorrect_value, third_incorrect_value, second_incorrect_value):
-        """Constructor"""
-        self.term = term
-        self.translation = translation
-        self.first_incorrect_value = first_incorrect_value
-        self.second_incorrect_value = second_incorrect_value
-        self.third_incorrect_value = third_incorrect_value
-
-    def get_term(self):
-        return self.term
-
-    def get_translation(self):
-        return self.translation
