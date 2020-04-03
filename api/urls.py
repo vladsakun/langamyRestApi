c5a984bc8210285dbab0d28b7bac738dd518c970
vlad <vlad12040@gmail.com>
2020-04-03 11:36:27 +0300

    class based dictation and user

--- api/urls.py
+++ api/urls.py
@@ -6,25 +6,21 @@ urlpatterns = [
 
     path('translate/<str:from_lang>/<str:to_lang>/<str:mode>/', views.translate),
 
-    path('get/studyset/<int:pk>/', views.study_set_detail),
     path('finish/studyset/<int:pk>/<str:mode>/', views.finish_study_set),
-    path('delete/studyset/<int:pk>/', views.study_set_detail),
-    path('patch/studyset/<int:pk>/', views.study_set_detail),
     path('clone/studyset/<int:id>/<str:email>/', views.clone_studyset),
     path('get/studysetsnames/<str:user_email>/', views.GetStudySetsOfCurrentUser.as_view()),
     path('get/shared/studyset/<str:creator>/<int:pk>/', views.get_shared_studyset),
 
-    path('create/dictation/', views.create_dictation),
-    path('get/dictation/<int:code>/<str:mode>/', views.dictation),
+    path('create/dictation/', views.DictationView.as_view()),
+    path('get/dictation/<int:code>/<str:mode>/', views.DictationView.as_view()),
+    path('delete/dictation/<int:pk>/', views.DictationView.as_view()),
+    path('get/dictationsnames/<str:user_email>/', views.GetDictationsOfCurrentUser.as_view()),
     path('get/dictation/marks/<int:dictation_id>/<str:mode>/', views.get_members_marks),
     path('get/user/completed/dictations/<str:email>/', views.get_user_completed_dictations),
-    path('delete/dictation/<int:code>/<str:mode>/', views.dictation),
-    path('patch/dictation/<int:code>/<str:mode>/', views.dictation),
-    path('get/dictationsnames/<str:user_email>/', views.GetDictationsOfCurrentUser.as_view()),
     path('get/random/dictation/<str:creator>/', views.RandomDictation.as_view()),
 
-    path('get/user/<str:email>/', views.user),
-    path('create/user/', views.user),
+    path('get/user/<str:email>/', views.User.as_view()),
+    path('create/user/', views.CreateUser.as_view()),
     path('patch/user/mark/<str:email>/', views.update_user_mark),
 
     path('studyset/<int:pk>/', views.StudySetDetail.as_view()),
--- api/views.py
+++ api/views.py
@@ -3,10 +3,12 @@ import json
 from django.http import JsonResponse
 from rest_framework import status, mixins, generics
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from yandex.Translater import Translater
-
 from api.serializers import *
+from .models import User as UserModel
 
 
 def createUniquId():
@@ -14,28 +16,8 @@ def createUniquId():
     return hash(str(uuid.uuid1())) % 100000000
 
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def study_set_detail(request, pk):
-    study_set = StudySets.objects.get(pk=pk)
-
-    if request.method == 'GET':
-        serializer = StudySetsSerializer(study_set)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT' or request.method == 'PATCH':
-        serializer = StudySetsSerializer(study_set, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        study_set.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 class CreateStudySet(mixins.CreateModelMixin,
                      generics.GenericAPIView):
-
     serializer_class = StudySetsSerializer
 
     def post(self, request, *args, **kwargs):
@@ -47,51 +29,57 @@ class StudySetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = StudySetsSerializer
 
 
-@api_view(['GET', 'PATCH', 'DELETE', 'POST'])
-def dictation(request, code, mode):
-    global dictation
-    if mode == "id":
-        dictation = Dictation.objects.get(id=code)
-    elif mode == "code":
-        dictation = Dictation.objects.get(code=code)
-    if request.method == 'GET':
-        serializer = SpecificDictationSerializer(dictation)
-        return Response(serializer.data)
-    elif request.method == 'PATCH':
-        serializer = SpecificDictationSerializer(dictation, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        dictation.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class GetDictationsOfCurrentUser(generics.ListAPIView):
+    serializer_class = DictationNamesSerializer
+
+    def get_queryset(self, user_email=None):
+        user_email = self.kwargs['user_email']
+        return Dictation.objects.filter(creator=user_email).order_by('-updated_at')
 
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE', 'POST'])
-def user(request, email="default"):
-    if request.method == 'GET':
-        user = User.objects.get(email=email)
-        serializer = UserSerializer(user)
+class GetStudySetsOfCurrentUser(generics.ListAPIView):
+    serializer_class = StudySetsSerializer
+
+    def get_queryset(self, user_email=None):
+        user_email = self.kwargs['user_email']
+        return StudySets.objects.filter(creator=user_email)
+
+
+class User(APIView):
+
+    def get(self, request, email=None, format=None):
+        lookup = {'email': email}
+        vessel = get_object_or_404(UserModel, **lookup)
+        serializer = UserSerializer(vessel, context={'request': request})
         return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
+
+
+class CreateUser(mixins.CreateModelMixin,
+                 generics.GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class DictationView(APIView):
+    def get(self, request, code=None, mode=None, format=None):
+        lookup = {'pk': code} if mode == 'id' else {'code': code}
+        dictation = get_object_or_404(Dictation, **lookup)
+        serializer = SpecificDictationSerializer(dictation, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SpecificDictationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(code=createUniquId())
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'PUT' or request.method == 'PATCH':
-        user = User.objects.get(email=email)
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        user = User.objects.get(email=email)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
+    def delete(self, request, pk=None, format=None):
+        dictation = Dictation.objects.get(pk=pk)
+        dictation.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 def get_members_marks(request, dictation_id, mode):
@@ -150,14 +138,6 @@ def get_shared_studyset(request, creator, pk):
         return Response(serializer.data)
 
 
-class GetDictationsOfCurrentUser(generics.ListAPIView):
-    serializer_class = DictationNamesSerializer
-
-    def get_queryset(self, user_email=None):
-        user_email = self.kwargs['user_email']
-        return Dictation.objects.filter(creator=user_email).order_by('-updated_at')
-
-
 @api_view(['POST'])
 def create_study_set(request):
     global serializer
@@ -178,14 +158,6 @@ def create_dictation(request):
             serializer.save(code=createUniquId())
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class GetStudySetsOfCurrentUser(generics.ListAPIView):
-    serializer_class = StudySetsSerializer
-
-    def get_queryset(self, user_email=None):
-        user_email = self.kwargs['user_email']
-        return StudySets.objects.filter(creator=user_email)
 
 
 @api_view(['POST', 'GET'])
